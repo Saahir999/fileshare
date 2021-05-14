767d22e6249b6e2343d72e3fc9893c38d15e34a8
Saahir999 <f20201142@goa.bits-pilani.ac.in>
2021-05-14 21:31:24 +0530

    first

---
+++ firebase/app/src/main/java/com/example/firebase/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.firebase
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val text = findViewById<TextView>(R.id.textView)
+        val testc = findViewById<Button>(R.id.testc)
+        testc.setOnClickListener {
+            text.text = "yes"
+            Intent(this , Sender::class.java).also{
+                startActivity(it)
+            }
+        }
+
+        }
+    }
